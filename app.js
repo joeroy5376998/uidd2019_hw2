8b64c807bcdf7ff9ba5ea2b09b5e26ad26522771
joeroy5376998 <joeroy5376998@gmail.com>
2019-03-16 22:22:21 +0800

    hw2

---
+++ app.js
@@ -0,0 +1,9 @@
+$(document).ready(function(){ 
+  $(".button").click(function(){
+    $(".button").animate({
+      left:'250px',
+      height:'+=150px',
+      width:'+=150px'
+    });
+  });
+});
